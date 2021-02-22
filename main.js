c78a2a226bfcb580b4779ecf87350661e7c895eb
Dijango Alves <dijango_ilha@hotmail.com>
2021-02-22 20:27:32 -0300

    remove console.log and comments

--- main.js
+++ main.js
@@ -1,35 +1,31 @@
 const produtos = [
-    {
-        id: 'abc123',
-        nome: 'JSRaiz para FW',
-        preco: 300,
-        descricao: 'O melhor curso JS',
-        imagem: 'http://lorempixel.com/500/300/'
-    },
-    {
-        id: 'bbc123',
-        nome: 'JSRaiz para Node',
-        preco: 1200,
-        descricao: 'O melhor curso JS',
-        imagem: 'http://lorempixel.com/500/300/'
-    },
-    {
-        id: 'cbc123',
-        nome: 'Programação funcional com JS',
-        preco: 500,
-        descricao: 'O melhor curso JS',
-        imagem: 'http://lorempixel.com/500/300/'
-    },
-];
+  {
+    id: 'abc123',
+    nome: 'JSRaiz para FW',
+    preco: 300,
+    descricao: 'O melhor curso JS',
+    imagem: 'http://lorempixel.com/500/300/'
+  },
+  {
+    id: 'bbc123',
+    nome: 'JSRaiz para Node',
+    preco: 1200,
+    descricao: 'O melhor curso JS',
+    imagem: 'http://lorempixel.com/500/300/'
+  },
+  {
+    id: 'cbc123',
+    nome: 'Programação funcional com JS',
+    preco: 500,
+    descricao: 'O melhor curso JS',
+    imagem: 'http://lorempixel.com/500/300/'
+  }
+]
 
-const carrinhoItens = {
+const carrinhoItens = {}
 
-};
-
-console.log(carrinhoItens)
-
-function renderizaProduto(produto, index) {
-    return `
+function renderizaProduto (produto, index) {
+  return `
         <div class="col-sm-4 mb-3">
             <div class="card">
                 <div class="card loja_item">
@@ -46,77 +42,81 @@ function renderizaProduto(produto, index) {
     `
 }
 
-function renderizaProdutos() {
-    return produtos.map((produto, i) => {
-        return renderizaProduto(produto, i)
-    }).join("");
-
-    // let html = '';
-    // for (let i = 0; i < produtos.length; i++){
-    //     html = html + renderizaProduto(produtos[i]);
-    // }
-    // return html;
+function renderizaProdutos () {
+  return produtos
+    .map((produto, i) => {
+      return renderizaProduto(produto, i)
+    })
+    .join('')
 }
-function renderizaItemCarrinho(produtoCarrinho) {
-    return `
+
+function renderizaItemCarrinho (produtoCarrinho) {
+  return `
     <div class="card carrinho_item mb-2">
         <div class="card-body">
             <h5 class="card-title">${produtoCarrinho.nome}</h5>
-            <p class="card-text">Preço unidade R$${produtoCarrinho.preco},00 | Quantidade: ${produtoCarrinho.quantidade}</p>
-            <p class="card-text">Valor: R$${produtoCarrinho.preco * produtoCarrinho.quantidade},00</p>
-            <button href="#" data-produto-id="${produtoCarrinho.id}" class="btn btn-danger btn-sm btn-remove">Remover</button>
+            <p class="card-text">Preço unidade R$${
+              produtoCarrinho.preco
+            },00 | Quantidade: ${produtoCarrinho.quantidade}</p>
+            <p class="card-text">Valor: R$${produtoCarrinho.preco *
+              produtoCarrinho.quantidade},00</p>
+            <button href="#" data-produto-id="${
+              produtoCarrinho.id
+            }" class="btn btn-danger btn-sm btn-remove">Remover</button>
         </div>
     </div>
     `
 }
 
-function renderizaCarrinho() {
-    let html = ''
-    for (let produtoId in carrinhoItens) {
-        html = html + renderizaItemCarrinho(carrinhoItens[produtoId]);
-    }
-    document.querySelector('.carrinho_itens').innerHTML = html;
+function renderizaCarrinho () {
+  let html = ''
+  for (let produtoId in carrinhoItens) {
+    html = html + renderizaItemCarrinho(carrinhoItens[produtoId])
+  }
+  document.querySelector('.carrinho_itens').innerHTML = html
 }
 
-function renderCarrinhoTotal() {
-    let total = 0;
-    for (let produtoId in carrinhoItens) {
-        total = total + (carrinhoItens[produtoId].preco * carrinhoItens[produtoId].quantidade);
-    }
-    document.querySelector('.carrinho_total')
-        .innerHTML = `<h6>Total: <strong>R$${total}</strong></h6>`
+function renderCarrinhoTotal () {
+  let total = 0
+  for (let produtoId in carrinhoItens) {
+    total =
+      total +
+      carrinhoItens[produtoId].preco * carrinhoItens[produtoId].quantidade
+  }
+  document.querySelector(
+    '.carrinho_total'
+  ).innerHTML = `<h6>Total: <strong>R$${total}</strong></h6>`
 }
 
-function adicionaItemNoCarrinho(produto) {
-    if (!carrinhoItens[produto.id]) {
-        carrinhoItens[produto.id] = produto;
-        carrinhoItens[produto.id].quantidade = 0;
-    }
-    ++carrinhoItens[produto.id].quantidade;
-    renderizaCarrinho();
-    renderCarrinhoTotal();
+function adicionaItemNoCarrinho (produto) {
+  if (!carrinhoItens[produto.id]) {
+    carrinhoItens[produto.id] = produto
+    carrinhoItens[produto.id].quantidade = 0
+  }
+  ++carrinhoItens[produto.id].quantidade
+  renderizaCarrinho()
+  renderCarrinhoTotal()
 }
 
-document.body
-    .addEventListener('click', function (event) {
-        const elemento = event.target;
+document.body.addEventListener('click', function (event) {
+  const elemento = event.target
 
-        if (elemento.classList.contains('btn-add')) {
-            const index = parseInt(elemento.getAttribute('data-index'), 10);
-            const produto = produtos[index];
-            adicionaItemNoCarrinho(produto);
-        }
+  if (elemento.classList.contains('btn-add')) {
+    const index = parseInt(elemento.getAttribute('data-index'), 10)
+    const produto = produtos[index]
+    adicionaItemNoCarrinho(produto)
+  }
 
-        if (elemento.classList.contains('btn-remove')) {
-            const produtoId = elemento.getAttribute('data-produto-id');
-            if (carrinhoItens[produtoId].quantidade <= 1) {
-                delete carrinhoItens[produtoId];
-            } else {
-                --carrinhoItens[produtoId].quantidade;
-            }
-            renderizaCarrinho();
-            renderCarrinhoTotal();
-        }
-    });
+  if (elemento.classList.contains('btn-remove')) {
+    const produtoId = elemento.getAttribute('data-produto-id')
+    if (carrinhoItens[produtoId].quantidade <= 1) {
+      delete carrinhoItens[produtoId]
+    } else {
+      --carrinhoItens[produtoId].quantidade
+    }
+    renderizaCarrinho()
+    renderCarrinhoTotal()
+  }
+})
 
-document.querySelector('.loja').innerHTML = renderizaProdutos();
+document.querySelector('.loja').innerHTML = renderizaProdutos()
